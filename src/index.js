6c4b929e507d7166d04a1d7c35b59cfa8602f55f
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-01-29 19:52:22 -0600

    Se agrego funcionalidad a cajas de texto y botones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-let text
+let text;
 
 function getText() {
-    text = getElementById("entry").value
-    getElementById("root").style.display = "none"
-    getElementById("screen1")style.display = "block"
+    text = document.getElementById("entry").value;
+    document.getElementById("root").style.display = "none";
+    document.getElementById("screen1").style.display = "block";
+    console.log("el texto recibido es " + text);
 }
 
-console.log("el texto recibido es " + text);
+function getKey() {
+    let number = document.getElementById("key").value;
+    //let result = encode(text,number);
+    document.getElementById("screen1").style.display ="none";
+    document.getElementById("screen2").style.display ="block";
+    console.log("el numero clave es " + number); 
+    //document.getElementById("out").value = result;
+    document.getElementById("origin").value = text; 
+    
+}
+
+function doAgain(){
+    document.getElementById("key").value = "";
+    document.getElementById("entry").value = "";
+    document.getElementById("screen2").style.display = "none";
+    document.getElementById("root").style.display = "block";
+
+}
