ac97a58407d8d6f8f2e1b8a5f1ed5f5aa3cfb8f2
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-01-30 21:54:24 -0600

    Funcion de cifrado y descifrado trabajando

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,34 @@
 window.cipher = {
   // ... 
  
+encode : function (text,number){
+  
+  let result = ''; 
+    
+  for (let i = 0; i < text.length; i++){
+    let knowPo = text.charCodeAt(i);// esto me regresa el 65, saber posicion puede ser knowPo
+    console.log(knowPo + ' ' + text.charAt(i));
+    let addNumber = ((knowPo - 65) + number)%26 + 65;// puede ser addNumber
+    console.log(addNumber); //esto solo lo necesito yo en realidad
+    let changeLe = String.fromCharCode(addNumber); 
+    result += changeLe; //esto combierte el resultado numerico de addnumber a letra y puede llamarse changeLe
+  }
+  return result; //aqui le estoy diciendo que me devuelva la letra(s) que cambio
+},
+
+decode : function (text,number){
+  
+  let result = ''; 
+    
+  for (let i = 0; i < text.length; i++){
+    let knowPo = text.charCodeAt(i);// esto me regresa el 65, saber posicion puede ser knowPo
+    console.log(knowPo + ' ' + text.charAt(i));
+    let addNumber = ((knowPo - 65) - number)%26 + 65;// puede ser addNumber
+    console.log(addNumber); //esto solo lo necesito yo en realidad
+    let changeLe = String.fromCharCode(addNumber); 
+    result += changeLe; //esto combierte el resultado numerico de addnumber a letra y puede llamarse changeLe
+  }
+  return result; //aqui le estoy diciendo que me devuelva la letra(s) que cambio
+}
+
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 let text;
 
 function getText() {
-    text = document.getElementById("entry").value;
+    text = document.getElementById("entry").value.toUpperCase();
     document.getElementById("root").style.display = "none";
     document.getElementById("screen1").style.display = "block";
     console.log("el texto recibido es " + text);
 }
 
-function getKey() {
+function getKey(magic) {
     let number = document.getElementById("key").value;
-    //let result = encode(text,number);
+    let result;  //=cipher.encode(text,parseInt(number));
+    if(magic === 1){
+      result = cipher.encode(text,parseInt(number));
+    }
+    else {
+      result = cipher.decode(text,parseInt(number));
+    }  
+    
     document.getElementById("screen1").style.display ="none";
     document.getElementById("screen2").style.display ="block";
     console.log("el numero clave es " + number); 
-    //document.getElementById("out").value = result;
+    document.getElementById("out").value = result;
     document.getElementById("origin").value = text; 
     
 }
